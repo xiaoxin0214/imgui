6b57498fb61515f0d3726a58d4f17e8836084b06
xinxiao <xinxiao0214@outlook.com>
2023-08-06 15:26:07 +0800

    添加premake编译脚本

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "ImGui"
+kind "StaticLib"
+language "C++"
+staticruntime "On"
+
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_internal.h",
+    "imgui_draw.cpp",
+    "imgui_tables.cpp",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp"
+}
+
+filter "system:windows"
+    systemversion "latest"
+
+filter "configurations:Debug"
+    runtime "Debug"
+    symbols "On"
+
+filter "configurations:Release"
+    runtime "Release"
+    optimize "On"
